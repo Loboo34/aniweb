f29d820f28dcb76c058780fd7d13be895a9a9fc2
loboo34 <rayovers40@gmail.com>
2024-09-23 20:54:51 +0300

    limiter

--- server/Routes/apiRoutes.js
+++ server/Routes/apiRoutes.js
@@ -1,115 +1,65 @@
 const express = require("express");
 const axios = require("axios");
-const rateLimit = require("express-rate-limit");
+const redis = require("redis");
 
-// Rate limit
-const limiter = rateLimit({
-  windowMs: 1000, // 10 minutes
-  max: 3, // limit each IP to 100 requests per windowMs
-  message:
-    "You have exceeded the 60 requests in 10 minutes limit! Please try again later.",
+const client = redis.createClient( {
+  url: 'redis://localhost:6379'
 });
+
+//top/anime?filter=upcoming
+
+//client.connect.catch();
 
 const baseUrl = "https://api.jikan.moe/v4";
 
 const router = express.Router();
 
-router.get("free/:id", async (req, res) => {
+//airing anime route
+router.get("/airing", async (req, res) => {
   try {
-    const response = await axios.get(`${baseUrl}/anime/{id}`);
-    res.json(response.data);
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-router.get("episode/:id", async (req, res) => {
-  try {
-    const response = await axios.get(`${baseUrl}/anime/{id}/episodes`);
-    res.json(response.data);
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-router.get("related/:id", async (req, res) => {
-  try {
-    const response = await axios.get(`${baseUrl}/anime/{id}/recommendations`);
-    res.json(response.data);
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-//airing
-router.get("/airing", limiter, async (req, res) => {
-  try {
-    const response = await axios.get(`${baseUrl}/top/anime?filter=airing`);
-    res.json(response.data);
+    const response = await axios.get(`${baseUrl}/top/anime?filter=airing&limit=10`);
+    const data = response.data;
+    res.json(data);
   } catch (error) {
     console.log(error);
   }
 });
 
 //popular
-router.get("/popular", limiter, async (req, res) => {
+router.get("/popular", async (req, res) => {
   try {
-    const response = await axios.get(
-      `${baseUrl}/top/anime?filter=bypopularity`
-    );
-    res.json(response.data);
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-router.get("/top", async (req, res) => {
-  try {
-    const response = await axios.get(`${baseUrl}/top/anime?filter=top`);
-    res.json(response.data);
+    const response = await axios.get(`${baseUrl}/top/anime?filter=bypopularity&limit=10`);
+    const data = response.data;
+    res.json(data);
   } catch (error) {
     console.log(error);
   }
 });
 
 //upcoming
-router.get("/upcoming", limiter, async (req, res) => {
+router.get("/upcoming", async (req, res) => {
   try {
-    const response = await axios.get(`${baseUrl}/top/anime?filter=upcoming`);
-    res.json(response.data);
+    const response = await axios.get(`${baseUrl}/top/anime?filter=upcoming&limit=10`);
+    const data = response.data;
+    res.json(data);
   } catch (error) {
     console.log(error);
   }
 });
 
 //completed
-router.get("/completed", async (req, res) => {
-  try {
-    const response = await axios.get(`${baseUrl}/top/anime?filter=completed`);
-    res.json(response.data);
-  } catch (error) {
-    console.log(error);
-  }
-});
 
 //recomended
-router.get("/recomended", limiter, async (req, res) => {
+router.get("/recomended", async (req, res) => {
   try {
-    const response = await axios.get(`${baseUrl}/recommendations/anime`);
-    res.json(response.data);
+    const response = await axios.get(`${baseUrl}/recomendations/anime&limit=10`);
+    const data = response.data;
+    res.json(data);
   } catch (error) {
     console.log(error);
   }
 });
 
 //genre
-router.get("/gernes", async (req, res) => {
-  try {
-    const response = await axios.get(`${baseUrl}/genres/anime`);
-    res.json(response.data);
-  } catch (error) {
-    console.log(error);
-  }
-});
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,12 +1,33 @@
 const cors = require("cors");
 const express = require("express");
 //const mongoose = require("mongoose");
+const rateLimit = require("express-rate-limit");
 const connectDb = require("./Config/dbConnection");
 require("dotenv").config();
 //const path = require('path')
+
+
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+const perSecondLimiter = rateLimit({
+  windowMs: 1000, // 1 second window
+  max: 3, // Limit each IP to 3 requests per second
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+// 60 requests per minute
+const perMinuteLimiter = rateLimit({
+  windowMs: 60 * 1000, // 1 minute window
+  max: 60, // Limit each IP to 60 requests per minute
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+app.use(perSecondLimiter);
+app.use(perMinuteLimiter);
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*"); // Allow requests from any origin
