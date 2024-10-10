2b2ec48cbb1b0915e52b2e8634cedf32c0ef2299
loboo34 <rayovers40@gmail.com>
2024-10-11 02:18:44 +0800

    something

--- frontend/src/pages/AZ-list.jsx
+++ frontend/src/pages/AZ-list.jsx
@@ -10,45 +10,36 @@ const AZlist = () => {
   //const link = footerLinks.find((link) => link.name === name);
   const [animeList, setAnimeList] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
+  const [selectedLetter, setSelectedLetter] = useState(letter);
   const [totalPages, setTotalPages] = useState(0);
   const [hasNextPage, setHasNextPage] = useState(false);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
   const limit = 25; // Number of items per page
-  const fetchWithBackoff = async (url, retries = 3, backoff = 500) => {
-    try {
-      const response = await fetch(url);
-      if (!response.ok) throw new Error("API Error");
-      return await response.json();
-    } catch (error) {
-      if (retries > 0) {
-        console.log(`Retrying in ${backoff} ms...`);
-        await new Promise((res) => setTimeout(res, backoff));
-        return fetchWithBackoff(url, retries - 1, backoff * 2); // Exponential backoff
-      }
-      throw error;
-    }
-  };
+ 
 
-  const getAnime = async (page) => {
-    setLoading(true);
-    const response = await axios.get(
-      `http://localhost:4000/api/anime/anime/${letter}?page=${page}&limit=${limit}`
-    );
-    const { pagination, data } = response.data;
-    setAnimeList(data);
-    console.log(data.data);
-    setCurrentPage(pagination.current_page);
-    setTotalPages(pagination.last_visible_page);
-    setHasNextPage(pagination.has_next_page);
+ 
+  
+  //   setLoading(true);
+  //   const response = await axios.get(
+  //     `http://localhost:4000/api/anime/anime/${letter}?page=${page}&limit=${limit}`
+  //   );
+  //   const { pagination, data } = response.data;
+  //   setAnimeList(data);
+  //   console.log(data.data);
+  //   setCurrentPage(pagination.current_page);
+  //   setTotalPages(pagination.last_visible_page);
+  //   setHasNextPage(pagination.has_next_page);
 
-    console.log(pagination);
-    setLoading(false);
-  };
-  useEffect(() => {
-    getAnime(currentPage);
-  }, [letter, currentPage]);
+  //   console.log(pagination);
+    
+  // };
+
+
+  // useEffect(() => {
+  //   getAnime(currentPage);
+  // }, [letter, currentPage]);
   // const filter = animeData.slice(0, 10)
   const config = {
     showTitle: true,
@@ -71,6 +62,45 @@ const AZlist = () => {
       setCurrentPage((prev) => prev - 1);
     }
   };
+const fetchAnime = async (page) => {
+  setLoading(true)
+    try {
+      const response = await axios.get(
+        `http://localhost:4000/api/anime/anime/${letter}?page=${page}&limit=${limit}`
+      );
+      const { pagination, data } = response.data;
+      setAnimeList(data);
+      setCurrentPage(pagination.current_page);
+      setTotalPages(pagination.last_visible_page);
+      setHasNextPage(pagination.has_next_page);
+      console.log(data);
+      console.log(pagination);
+      setLoading(false);
+    } catch (error) {
+      setError(error);
+    }finally{
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => { 
+    //setLoading(true)
+    fetchAnime(currentPage, selectedLetter)
+  }, [letter, currentPage, selectedLetter]);
+
+  const handleLetterChange = (letter) => {
+    setSelectedLetter(letter);
+    setCurrentPage(1);
+  }
+
+  //function to generate page numbers based on total pages
+  const generatePageNumbers = () => {
+    const pages = [];
+    for (let i = 1; i <= totalPages; i++) {
+      pages.push(i);
+    }
+    return pages;
+  };
 
   return (
     <div className="pt-[55px] md:pl-4  relative">
@@ -85,43 +115,43 @@ const AZlist = () => {
             className={`md:text-[1.3rem] text-[.8rem] pt-1 pb-1 cursor-deafult  text-white pl-2 pr-2 rounded mr-2 bg-gray-600  font-medium 
             `}
             key={link.id}
+            onClick={() => handleLetterChange(link.letter)}
           >
             {link.letter}
           </NavLink>
         ))}
       </div>
       <>
-        <div className="md:grid md:grid-cols-5">
-          {animeList.map((anime) => (
-            <Suspense key={anime.id} fallback={<div>Loading...</div>}>
-              <Card anime={anime} config={config} />
-            </Suspense>
-          ))}
-        </div>
+        {loading ? (
+          <div>Loading...</div>
+        ) : (
+          <div className="md:grid md:grid-cols-5 cardContainer">
+            {animeList.map((anime) => (
+              <Suspense key={anime.id} fallback={<div>Loading...</div>}>
+                <Card anime={anime} config={config} />
+              </Suspense>
+            ))}
+          </div>
+        )}
       </>
       {loading && <div>Loading...</div>}
-      <p className=" text-white">
-        {" "}
-        Page {currentPage} of {totalPages}{" "}
-      </p>
-      {!loading && (
-        <div className=" text-white">
-          <button
-            onClick={handlePreviousPage}
-            disabled={currentPage <= 1}
-            className="bg-[#00a2ff] text-white pl-2 pr-2 pt-1 pb-1 rounded"
-          >
-            Previous
-          </button>
-          <button
-            onClick={handleNextPage}
-            disabled={!hasNextPage}
-            className="bg-[#00a2ff] text-white pl-2 pr-2 pt-1 pb-1 rounded"
-          >
-            Next
-          </button>
-        </div>
-      )}
+     
+      
+     <div>
+    {generatePageNumbers().map((page) => (
+      <button
+        key={page}
+        onClick={() => setCurrentPage(page)}
+        className={`${
+          currentPage === page ? "bg-gray-500" : "bg-gray-300"
+        } text-white font-semibold text-lg p-2 rounded-lg`}
+      >
+        {page}
+      </button>
+    ))}
+     </div>
+
+    
     </div>
   );
 };
